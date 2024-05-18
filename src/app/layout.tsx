9ac1600afee59da1e2b8d164aa88d3bcbdff6108
Lepre-CHAU-n <71118130+ChauHPham@users.noreply.github.com>
2024-05-18 16:25:29 -0700

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" /> {/* Reference to the favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
