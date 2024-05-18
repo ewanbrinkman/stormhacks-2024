9a957c8a62744dcbade6e23d5cc4a22926a75059
Lepre-CHAU-n <71118130+Lepre-CHAU-n@users.noreply.github.com>
2024-05-18 14:37:59 -0700

    Create page.tsx

---
+++ src/app/resource/page.tsx
@@ -0,0 +1,9 @@
+export default function Resource() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex flex-grow relative justify-center items-center">
+          <h1>Hello there!</h1>
+        </div>
+      </main>
+    );
+  }
