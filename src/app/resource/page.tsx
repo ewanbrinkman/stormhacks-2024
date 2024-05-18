02f3a108aa8cae13b538f19957a59a537ed72b88
Lepre-CHAU-n <71118130+ChauHPham@users.noreply.github.com>
2024-05-18 14:38:51 -0700

    Update page.tsx

--- src/app/resource/page.tsx
+++ src/app/resource/page.tsx
@@ -2,7 +2,7 @@ export default function Resource() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="flex flex-grow relative justify-center items-center">
-          <h1>Hello there!</h1>
+          <h1>Hello thered!</h1>
         </div>
       </main>
     );
