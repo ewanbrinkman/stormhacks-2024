718fc6dd7b43365ab6257610cca1aa8dbb0a93ea
Ewan Brinkman <ewanbrinkman@gmail.com>
2024-05-18 19:58:42 -0700

    fix: stop navbar focus highlight staying

--- src/app/components/NavbarLink.tsx
+++ src/app/components/NavbarLink.tsx
@@ -9,7 +9,7 @@ const NavbarLink: React.FC<NavbarLinkProps> = (props: NavbarLinkProps) => {
     return (
         <Link
             href={props.href}
-            className="text-white text-xl font-semibold mx-8 transition duration-300 ease-in-out hover:text-gray-300 focus:text-gray-400"
+            className="text-white text-xl font-semibold mx-8 transition duration-300 ease-in-out hover:text-gray-300"
         >
             {props.children}
         </Link>
